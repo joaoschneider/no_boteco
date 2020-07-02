4156775e4698792e7b2f119869dcc50625d9e0d9
mr-schneider1 <joaoschneider1007@live.com>
2020-07-02 14:30:35 -0300

    feature:
    login implementado para email e senha

--- app/src/main/java/com/noboteco/noboteco/login.java
+++ app/src/main/java/com/noboteco/noboteco/login.java
@@ -1,5 +1,6 @@
 package com.noboteco.noboteco;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,29 +8,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
-
+    FirebaseAuth mAuth;
+    // Dados do usuário para serem usados
+    EditText mEditUsername;
+    EditText mEditPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        mAuth = FirebaseAuth.getInstance();
 
         Button btnentrar = findViewById(R.id.login);
-        EditText edtusername = findViewById(R.id.editusername);
-        EditText edtpassword = findViewById(R.id.editpassword);
-
-        // Dados do usuário para serem usados
-
-        String username = edtusername.getText().toString();
-        String password = edtpassword.getText().toString();
-
+        mEditUsername = findViewById(R.id.editusername);
+        mEditPassword = findViewById(R.id.editpassword);
 
         btnentrar.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                changeActivity();
+                String username = mEditUsername.getText().toString();
+                String password = mEditPassword.getText().toString();
+                mAuth.signInWithEmailAndPassword(username,password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                changeActivity();
+                                //TODO: adicionar ao intent a informação de userID para uso futuro do banco de dados
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(login.this, "Verifique os dados informados e tente novamente.", Toast.LENGTH_LONG).show();
+                            }
+                        });
             }
         });
 
