6917fdb56ad61c66a85291751be7d0f3acdbe349
Pablo Yuri Scherer de Souza <pabloyuriss@hotmail.com>
2020-07-02 00:48:24 -0300

    Troca de activities via intents.

--- app/src/main/java/com/noboteco/noboteco/MainActivity.java
+++ app/src/main/java/com/noboteco/noboteco/MainActivity.java
@@ -2,7 +2,10 @@ package com.noboteco.noboteco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_entrada);
+
+        Button meuperfilbtn = findViewById(R.id.meu_perfil);
+        Button entrarbarbtn = findViewById(R.id.entrar_no_bar);
+
+
+
+
+        meuperfilbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoperfil();
+            }
+        });
+
+        entrarbarbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotobar();
+            }
+        });
+
+
     }
+        public void gotoperfil(){
+            Intent goperfil = new Intent(this, perfil.class);
+            startActivity(goperfil);
+        }
+
+        public void gotobar(){
+            Intent gobar = new Intent(this, leitor_cod_qr.class);
+            startActivity(gobar);
+        }
+
+
+
+
 }
---
+++ app/src/main/java/com/noboteco/noboteco/perfil.java
@@ -0,0 +1,14 @@
+package com.noboteco.noboteco;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil);
+    }
+    }
